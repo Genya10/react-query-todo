96414bc33393d3305dca29a24ff65c772857c60c
Jenya <113167825+Genya10@users.noreply.github.com>
2024-07-09 13:57:37 +0300

    add useMutation and useQueryClient in NewTodo.tsx

--- src/components/NewTodo.tsx
+++ src/components/NewTodo.tsx
@@ -1,22 +1,34 @@
 import { Button, Input, Stack } from "@chakra-ui/react";
 import { useState } from "react";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { createTodo } from "../services/todos-service";
 
 const NewTodo = () => {
-  const [title, setTiile] = useState("");
+  const [title, setTitle] = useState("");
+
+  const client = useQueryClient();
+
+  const { mutate } = useMutation({
+    mutationFn: (title: string) => createTodo(title),
+    onSuccess: () => {
+      client.invalidateQueries({ queryKey: ["todos", "all"] });
+    },
+  });
 
   const submitHandler: React.FormEventHandler<HTMLFormElement> = (event) => {
     event.preventDefault();
     if (title) {
-      setTiile("");
+      mutate(title);
+      setTitle("");
     }
   };
 
   return (
     <form onSubmit={submitHandler}>
-      <Stack>
+      <Stack direction="row">
         <Input
           value={title}
-          onChange={(e) => setTiile(e.target.value)}
+          onChange={(e) => setTitle(e.target.value)}
           placeholder="new todo..."
         />
         <Button type="submit">Add todo</Button>
--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -4,16 +4,16 @@ import { toggleTodoStatus } from "../services/todos-service";
 import { TodoType } from "../types/todo";
 
 const TodoItem = ({ completed, id, title }: TodoType) => {
-  const client = useQueryClient();
+  /*const client = useQueryClient();
 
   const { mutate: toggle } = useMutation({
     mutationFn: () => toggleTodoStatus(id, !completed),
     onSuccess: () => client.invalidateQueries({queryKey:['todos']}),
-  });
+  });*/
 
   return (
     <ListItem>
-      <Stack spacing={4} direction="row" onClick={() => toggle()}>
+      <Stack spacing={4} direction="row" >
         <Checkbox isChecked={completed}>{title}</Checkbox>
       </Stack>
     </ListItem>
@@ -21,3 +21,5 @@ const TodoItem = ({ completed, id, title }: TodoType) => {
 };
 
 export { TodoItem };
+
+//onClick={() => toggle()}
