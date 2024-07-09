669141cc718a5f10a9634cf9c91cd845c4a499db
Jenya <113167825+Genya10@users.noreply.github.com>
2024-07-09 10:51:19 +0300

    create components: TodoItem.tsx, TodoList.tsx.

---
+++ src/components/TodoList.tsx
@@ -0,0 +1,31 @@
+import {Spinner, List} from '@chakra-ui/react'
+import { TodoType } from '../types/todo'
+import { TodoItem } from './TodoItem'
+
+const TodoList = ()=>{
+
+const isLoading = true;
+const data: TodoType[] = []
+
+if(isLoading){
+   return (
+    <Spinner 
+      thickness='4px'
+      speed='0.65s'
+      emptyColor='gray.200'
+      color='blue.500'
+      size='xl'
+    />
+   )
+};
+
+ return (
+    <List>
+        {data.map((todo)=>(
+         <TodoItem key={todo.id} {...todo}/>
+        ))}
+    </List>
+ )
+}
+
+export {TodoList}
